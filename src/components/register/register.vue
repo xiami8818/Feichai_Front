e4bf9b9927980c7ca82146fdd89228f084cff848
xiami <2367386211@qq.com>
2020-08-18 22:05:16 +0800

    8.18第三次提交(部署live2d，调整了登录页面，完成登录功能，压缩字体库，调整注册页面)

--- src/components/register/register.vue
+++ src/components/register/register.vue
@@ -1,20 +1,12 @@
 <template>
     <div id="register">
         <h2 style="text-align: center">欢迎注册废柴工作室</h2>
-        <div class="resInfo">
-            <input type="text" v-model="name" name="username" lay-verify="required" placeholder="请输入您的姓名" autocomplete="off" class="layui-input" >
-        </div>
-        <div class="resInfo">
-            <input type="text" v-model="phone" name="username" lay-verify="required" placeholder="请输入您的手机号" autocomplete="off" class="layui-input">
-        </div>
-        <div class="resInfo">
-            <input type="text" v-model="password" name="username" lay-verify="required" placeholder="请设置您的密码" autocomplete="off" class="layui-input">
-        </div>
-        <div class="resInfo">
-            <input type="text" v-model="confirmPwd" name="username" lay-verify="required" placeholder="请确认您的密码" autocomplete="off" class="layui-input">
-          <img src="../../assets/right.jpg" v-if="confirm&confirmPwd!=''" />
-        </div>
-        <button>注册</button>
+            <input type="text" v-model="name" id="name" name="username" lay-verify="required" placeholder="请输入您的姓名" autocomplete="off" class="layui-input" >
+            <input type="text" v-model="phone" id="phone" name="username" lay-verify="required" placeholder="请输入您的手机号" autocomplete="off" class="layui-input">
+            <input type="text" v-model="password" id="password" name="username" lay-verify="required" placeholder="请设置您的密码" autocomplete="off" class="layui-input">
+            <input type="text" v-model="confirmPwd" id="confirmPwd" name="username" lay-verify="required" placeholder="请确认您的密码" autocomplete="off" class="layui-input">
+          <img src="../../assets/right.png" v-if="confirm&confirmPwd!=''" />
+        <p @click="register" id="commit">注册</p>
     </div>
 </template>
 <script>
@@ -68,37 +60,66 @@
     #register {
         position: absolute;
         top: 18vh;
-        width: 50vw;
+        width: 36vw;
         height: 68vh;
-        left: 25vw;
+        left: 32vw;
         background-color: #bff7ff;
         margin: 0 auto;
         border-radius: 2rem;
         z-index: 2;
     }
-    .resInfo {
-        margin-top: 2rem;
+    #commit{
+      position: absolute;
+      width: 80%;
+      left: 10%;
+      top: 70%;
+      height: 3rem;
+      border-radius: 4rem;
+      text-align: center;
+      background-color: antiquewhite;
+      box-shadow: 0 25px #ffda00,
+      0  35px rgba(0,0,0,0.3);
+      font-size: 1.4rem;
+      cursor: pointer;
+      transition: 0.2s linear;
     }
-    .resInfo input {
-        position: relative;
-        width: 68%;
-        height: 2.5rem;
-        border-radius: 0.2rem;
-        border-style: none;
-        margin-left: 4.5rem;
+    #commit:active {
+      transform: translateY(20px);
+      box-shadow: 0 5px 0 #ffda00,
+      0 15px 5px rgba(0,0,0,0.3);
     }
-    #register button{
-        position: relative;
-        width: 80%;
-        left: 10%;
-        margin-top: 2rem;
-        height: 3rem;
-        border-radius: 4rem;
-        outline-style: none;
-        border-style: none;
+    #register img {
+      position: absolute;
+      width: 40px;
+      left: 82%;
+      top: 60%;
     }
-    .resInfo img {
-      position: relative;
-      top: 4vh;
+    #name {
+      position: absolute;
+      width: 60%;
+      height: 7%;
+      left: 20%;
+      top:  15%;
+    }
+    #phone {
+      position: absolute;
+      width: 60%;
+      height: 7%;
+      left: 20%;
+      top: 30%;
+    }
+    #password {
+      position: absolute;
+      width: 60%;
+      height: 7%;
+      left: 20%;
+      top: 45%;
+    }
+    #confirmPwd {
+      position: absolute;
+      width: 60%;
+      height: 7%;
+      left: 20%;
+      top: 60%;
     }
 </style>
